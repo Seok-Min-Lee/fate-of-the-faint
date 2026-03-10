13cddc781f73600a360de00cff07b59ff919ecaf
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2026-03-10 23:33:33 +0900

    상단 메뉴 고도화

---
+++ Assets/Scripts/FloorView.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FloorView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    private void Start()
+    {
+        text.text = PlayManager.Instance.MapGraph.LatestNode.Floor.ToString();
+    }
+}
---
+++ Assets/Scripts/UserNameView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class UserNameView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text; 
+    private void Start()
+    {
+        text.text = Environment.MachineName;
+    }
+}
