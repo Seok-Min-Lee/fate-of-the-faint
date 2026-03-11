15311c10e86ac257bd0e846189f1c446007e3598
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2026-03-12 02:31:36 +0900

    전투 UI 툴팁 임시 추가

---
+++ Assets/Scripts/TooltipTarget.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEngine.GraphicsBuffer;
+
+public class TooltipTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private RectTransform canvas;
+    [SerializeField] private TooltipView tooltip;
+    [SerializeField] private string head;
+    [SerializeField] private string description;
+    private RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.Bind(head, description);
+
+        tooltip.RectTransform.pivot = rectTransform.pivot;
+        tooltip.RectTransform.anchorMin = rectTransform.anchorMin;
+        tooltip.RectTransform.anchorMax = rectTransform.anchorMax;
+
+        tooltip.RectTransform.anchoredPosition = new Vector2(
+            rectTransform.anchoredPosition.x,
+            rectTransform.anchoredPosition.y + rectTransform.sizeDelta.y
+        );
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.Clear();
+    }
+}
